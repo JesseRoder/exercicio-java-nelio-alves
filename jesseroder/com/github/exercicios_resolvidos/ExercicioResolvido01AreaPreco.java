01bdf2dbf78786a1ee85bee49ace5df8a2c82722
Jesse Roder <gesseroder@gmail.com>
2026-05-28 15:55:17 +0100

    exercicio resolvido 01

---
+++ jesseroder/com/github/exercicios_resolvidos/ExercicioResolvido01AreaPreco.java
@@ -0,0 +1,31 @@
+/* Fazer um programa para ler as medidas da largura e comprimento de um terreno retangular com uma casa decimal, bem como o valor do metro quadrado do terreno com duas casas decimais. Em seguida, o programa deve mostrar o valor da área do terreno, bem como o valor do preco do terreno, ambos com duas casas decimais. */
+
+package jesseroder.com.github.exercicios_resolvidos;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ExercicioResolvido01AreaPreco {
+    public static void main(String[] args) {
+        
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite a largura do terreno: ");
+        double largura = sc.nextDouble();
+
+        System.out.print("Digite o comprimento do terreno: ");
+        double comprimento = sc.nextDouble();
+
+        System.out.print("Digite o valor do metro quadrado: ");
+        double valorMetroQuadrado = sc.nextDouble();
+
+        double area = largura * comprimento;
+        double preco = area * valorMetroQuadrado;
+
+        System.out.println();
+        System.out.printf("Área do terreno: %.2f%n", area);
+        System.out.printf("Preço do terreno: %.2f%n", preco);
+        sc.close();
+    }
+}
